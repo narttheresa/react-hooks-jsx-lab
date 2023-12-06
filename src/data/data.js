06ec03b123bfacb0897aed861a62a53ec90b8327
Theresa Tran <theresa1194@gmail.com>
2023-12-06 20:22:52 +1100

    done

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
+const name = "Theresa";
+const city = "Sydney";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
